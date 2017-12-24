85c5408c57b98e1893b445aa6c43b8b5b89b2558
Ariel Ross <arielross92@gmail.com>
2017-12-24 17:13:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ function whisper(string)
   return string.toLowerCase()
 }
 
-logShout
+function logShout(string)
+{
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase)
+}
